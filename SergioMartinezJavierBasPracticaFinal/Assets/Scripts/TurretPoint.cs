e70e18af2646b23e122b69435ce17dd21e023f6f
xMartu <38779343+xMartu@users.noreply.github.com>
2021-05-31 19:24:01 +0200

    gsdfgsdf

---
+++ SergioMartinezJavierBasPracticaFinal/Assets/Scripts/TurretPoint.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretPoint : MonoBehaviour
+{
+    public Material freeMat, selectedMat, occupiedMat;
+    public MeshRenderer meshR;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        
+    }
+}
