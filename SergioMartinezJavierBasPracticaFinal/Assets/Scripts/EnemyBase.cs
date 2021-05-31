9d9a3d9edd310171a1661f1dc3c93f3a4032a585
xMartu <38779343+xMartu@users.noreply.github.com>
2021-05-31 15:55:28 +0200

    gsdfg

---
+++ SergioMartinezJavierBasPracticaFinal/Assets/Scripts/EnemyBase.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBase : MonoBehaviour
+{
+    public float speed;
+    public float damage;
+    public float maxHP;
+    float currentHP;
+
+
+    public bool canMove;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
