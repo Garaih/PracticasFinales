46d62eff69f9356e588fc29d69d0fb037b8e0e21
xMartu <38779343+xMartu@users.noreply.github.com>
2021-05-31 16:14:23 +0200

    Update EnemyBase.cs

--- SergioMartinezJavierBasPracticaFinal/Assets/Scripts/EnemyBase.cs
+++ SergioMartinezJavierBasPracticaFinal/Assets/Scripts/EnemyBase.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class EnemyBase : MonoBehaviour
 {
+    public Transform[] pathPoints;
+    int currentPoint;
+
+    Rigidbody rb;
+
     public float speed;
+    public float acceleration;
+    public float turnDistance;
     public float damage;
     public float maxHP;
     float currentHP;
@@ -13,11 +20,45 @@ public class EnemyBase : MonoBehaviour
 
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        
+        if (canMove)
+        {
+            if (Vector3.Distance(transform.position, pathPoints[currentPoint].position) > turnDistance)
+            {
+                Vector3 desiredSpeed = (pathPoints[currentPoint].position - transform.position).normalized * speed;
+
+                rb.velocity = Vector3.MoveTowards(rb.velocity, desiredSpeed, acceleration * Time.deltaTime);
+            }
+            else
+            {
+                currentPoint++;
+
+                if (currentPoint >= pathPoints.Length)
+                {
+                    currentPoint = 0;
+                }
+            }
+        }
+    }
+
+    public void SetPath(Transform[] array) 
+    {
+        pathPoints = array;
+
+        canMove = true;
+    }
+
+    public void TakeDamage(float value)
+    {
+        currentHP -= value;
+
+        if (currentHP <= 0)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
